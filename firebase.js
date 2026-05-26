b077b0e97c74b3c6373bbd10e3d217f1daa2b764
ASTRO-Bugil <yunarchive0120@gmail.com>
2026-05-26 14:07:58 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,52 @@
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut
+} from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  serverTimestamp
+} from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+  apiKey: "YOUR_API_KEY",
+
+  authDomain: "YOUR_DOMAIN",
+
+  projectId: "YOUR_PROJECT_ID",
+
+  storageBucket: "YOUR_BUCKET",
+
+  messagingSenderId: "YOUR_SENDER_ID",
+
+  appId: "YOUR_APP_ID"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const provider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
+
+export {
+  signInWithPopup,
+  signOut,
+  collection,
+  addDoc,
+  getDocs,
+  serverTimestamp
+};
